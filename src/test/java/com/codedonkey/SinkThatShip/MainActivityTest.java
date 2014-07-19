a664a9ede38c5db0e25c86e6aa58e6d93d3ee89e
Mandy Choi <hello@mandychoi.com.au>
2014-07-19 16:10:45 +0100

    #0 initial commit - hello world and unit test

---
+++ src/test/java/com/codedonkey/SinkThatShip/MainActivityTest.java
@@ -0,0 +1,13 @@
+package com.codedonkey.SinkThatShip;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainActivityTest {
+    @Test
+    public void testThis() {
+        assertTrue(true);
+    }
+
+}
